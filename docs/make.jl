fe76ee30cd8a5e71d4cbe76238b2fe4a86a81497
YabusameHoulen <yabusamehour@gmail.com>
2024-05-10 17:28:26 +0800

    emm

--- docs/make.jl
+++ docs/make.jl
@@ -7,22 +7,22 @@ using MyDocs
 makedocs(;
     modules=[MyDocs],
     authors="YabusameHoulen <yabusamehour@gmail.com> and contributors",
-    repo="https://github.com/YabusameHoulen/MyDocs",
+    repo="https://github.com/YabusameHoulen/MTDocs.jl",
     sitename="MyDocs",
     format=DocumenterVitepress.MarkdownVitepress(;
-        repo="https://github.com/YabusameHoulen/MyDocs",
-        devurl = "dev",
-        deploy_url = "YabusameHoulen.github.io/MyDocs",
+        repo="https://github.com/YabusameHoulen/MTDocs.jl",
+        devurl="dev",
+        deploy_url="YabusameHoulen.github.io/MTDocs.jl",
     ),
     pages=[
         "Home" => "index.md",
     ],
-    warnonly = true,
+    warnonly=true,
 )
 
 
 deploydocs(;
-    repo="github.com/YabusameHoulen/MyDocs.git",
-    devbranch = "master",
+    repo="github.com/YabusameHoulen/MTDocs.git",
+    devbranch="master",
     push_preview=true,
 )
